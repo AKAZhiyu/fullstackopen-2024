6df8e11325d00a96a49ac2e19588fe4a5e83e7d0
AKAZhiyu <zhixuan.z2003@gmail.com>
2024-12-21 10:36:38 +0800

    finish Exercises 1.6.-1.7

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+
+const Header = ({text}) => <h1>{text}</h1>
+  
+const Button = ({text, onClick}) => {
+
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+// const FeedbackPanel = ({panelText1, panelText2, panelText3}) => {
+
+// }
+
+const FeedbackItem = ({text, num}) => {
+  return (
+    <div>
+      {text} {num}
+    </div>
+  )
+}
+
+const DisplayFeedback = ({goodNum, neutralNum, badNum}) => {
+  const total = goodNum + neutralNum + badNum
+  const average = ((goodNum + (-1 * badNum)) / total).toFixed(2)
+  const positiveRate = total === 0 ? "0%" : (goodNum / total * 100).toFixed(2) + "%"
+  return (
+    <div>
+      <h2>statistics</h2>
+      <FeedbackItem text={"good"} num={goodNum} />
+      <FeedbackItem text={"neutral"} num={neutralNum} />
+      <FeedbackItem text={"bad"} num={badNum} />
+      <FeedbackItem text={"all"} num={total} />
+      <FeedbackItem text={"average"} num={average} />
+      {/* <FeedbackItem text={"positive"} num={`${positiveRate}%`}  /> */}
+      <FeedbackItem text={"positive"} num={positiveRate}  />
+    </div>
+  )
+}
+
+const App = () => {
+  const siteTitle = "give feedback"
+
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header text={siteTitle} />
+      <Button text="good" onClick={handleGood} />
+      <Button text="neutral" onClick={handleNeutral} />
+      <Button text="bad" onClick={handleBad} />
+      <DisplayFeedback goodNum={good} neutralNum={neutral} badNum={bad} />
+    </div>
+  )
+}
+
+export default App
