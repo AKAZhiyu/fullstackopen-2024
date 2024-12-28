e4cfa75677ce289bc4d0a759102feb9758033c36
AKAZhiyu <zhixuan.z2003@gmail.com>
2024-12-28 17:55:11 +0800

    finish exercise 2.20

--- part2/countries/src/components/Display.jsx
+++ part2/countries/src/components/Display.jsx
@@ -1,13 +1,26 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const Display = ({ countries, setCountries }) => {
 
     const handleShow = (country) => {
         setCountries([country])
     }
 
+    const [weather, setWeather] = useState(null);
+
     if (countries.length === 0) {
         return <div>not found</div>
     } else if (countries.length === 1) {
         const country = countries[0];
+        const weather_api_key = import.meta.env.VITE_WEATHER_KEY;
+        const [lat, lon] = country.latlng;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_api_key}`
+        axios.get(url)
+             .then(res => {
+                setWeather(res.data)
+             })
+
         return (
             <div>
                 <h1>{country.name.common}</h1>
@@ -18,6 +31,19 @@ const Display = ({ countries, setCountries }) => {
                     {Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>)}
                 </ul>
                 <img src={country.flags.png} alt={`national flag of ${country.name.common}`}></img>
+                <h2>Weather in {country.capital}</h2>
+                {weather ? (
+                    <div>
+                        <div>{weather.weather[0].main}:  {weather.weather[0].description}</div>
+                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+                        <div>Temperature: {weather.main.temp.toFixed(2)} Celcius</div>
+                        <div>Wind: {weather.wind.speed} m/s</div>
+                    </div>
+                ) : (
+                    <div>Loading weather...</div>
+                )}
+                {/* https://api.openweathermap.org/data/2.5/weather?lat=22&lon=33&appid=d4162d7ba6b2a382658726aabb8cb41e */}
+                    
             </div>
         );
     } else if (countries.length > 10) {
